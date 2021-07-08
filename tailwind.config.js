c7513275bd31c798348f28ca01eb273dd0e4cf27
rkiel <cygnus@pobox.com>
2021-07-08 08:35:32 -0400

    setup: added custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [
     './*.html'
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        orange: colors.orange,
+        teal: colors.teal,
+        'light-blue': colors.lightBlue
+      }
+    },
   },
   variants: {
     extend: {},
